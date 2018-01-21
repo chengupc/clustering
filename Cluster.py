641276c9d9e9cc30316d63ad9ffe2e54c00ecad4
chengch <33085512+chengupc@users.noreply.github.com>
2018-01-21 21:45:24 +0800

    用来做cluster的算法，显示测试，之后进行mnist数据集进行测试。然后进行降维，再做测试

---
+++ Cluster.py
@@ -0,0 +1,10 @@
+from sklearn.datasets.samples_generator import make_blobs
+X, y = make_blobs(n_samples=300, centers=4,
+                  random_state=0, cluster_std=0.60)
+plt.scatter(X[:, 0], X[:, 1], s=50)
+
+from sklearn.cluster import KMeans
+est = KMeans(4)  # 4 clusters
+est.fit(X)
+y_kmeans = est.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='rainbow')
